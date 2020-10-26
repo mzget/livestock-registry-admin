7e25e30c04b551647aad8a4fb7fb65dbd3df8738
mzget <rattajak.n@gmail.com>
2020-10-26 11:00:05 +0700

    add feathers client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { restClient, authClient } from 'ra-data-feathers';
+
+import feathersClient from 'libs/feathersClient';
 
 import './App.css';
 
+const restClientOptions = {
+    id: '_id', // In this example, the database uses '_id' rather than 'id'
+    usePatch: true, // Use PATCH instead of PUT for updates
+};
+
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={restClient(feathersClient, restClientOptions)}>
         <Resource name="users" list={ListGuesser} />
     </Admin>
 );

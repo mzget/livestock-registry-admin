c18eb1538c73dc781f5547567e65e6b948f616be
mzget <rattajak.n@gmail.com>
2020-10-26 18:38:02 +0700

    update linter

--- src/Libs/feathersClient.ts
+++ src/Libs/feathersClient.ts
@@ -9,6 +9,12 @@ const restClient = rest('http://localhost:3030');
 
 // Configure an AJAX library (see below) with that client
 app.configure(restClient.fetch(window.fetch));
+// Use localStorage to store our login token
+app.configure(
+    feathers.authentication({
+        storage: window.localStorage,
+    })
+);
 
 // Connect to the `http://feathers-api.com/messages` service
 const users = app.service('users');
